f9b8caaf6376703545b1dfa753ce3aa26d4bbdf8
thestrangerd <ashleyseto25@gmail.com>
2025-04-29 04:06:31 -0700

    game titles

--- check-yourself/script.js
+++ check-yourself/script.js
@@ -3,8 +3,9 @@
 (function(){
     'use strict';
 
-    const gameContainer = document.querySelector('.month');
+    const gameContainer = document.querySelectorAll('.month');
     const monthName = document.querySelector('#month-name');
+    const gameName = document.querySelector('#game-name');
     
 
     async function getData() {
@@ -12,6 +13,8 @@
         const content = await data.json();
 
         console.log(content);
+        monthTitle(content);
+        gameTitle(content);
     }
 
     
@@ -20,11 +23,23 @@
             'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'
         ];
 
-        for (let i = 0; i < content.length; i++) {
-            const monthLabel = monthNames.length - 1;
+        for (let i = 0; i < gameContainer.length; i++) {
+            const monthNumber = content[i].month;
+            const monthLabel = monthNames[monthNumber - 1];
         
-            gameContainer.addEventListener('mouseenter', function() {
+            gameContainer[i].addEventListener('mouseenter', function() {
                 monthName.textContent = monthLabel;
+            })
+        }
+    }
+
+
+    function gameTitle(content) {
+        for (let i = 0; i < gameContainer.length; i++) {
+            const title = content[i].title;
+
+            gameContainer[i].addEventListener('mouseenter', function() {
+                gameName.textContent = `Most Played: ${title}`;
             })
         }
     }
